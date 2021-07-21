d96c7d1055c5686f13d2fa19f811e37aa9156e39
thisisanorak <87737728+thisisanorak@users.noreply.github.com>
2021-07-21 13:46:09 -0500

    update

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ var dbd = require("dbd.js")
 var fs = require("fs")
 
 const bot = new dbd.Bot({
-token: "TOKEN",
+token: "ODY3MTc2MDMxNzc3MzI1MDY2.YPdSnQ.lNMFjzSTyf44saFmDuAlWOKSbAw",
 prefix: "$getServerVar[prefix]"
 })
  
 bot.status({
-  text: "YOUR BOT BIO",
+  text: "| 🏁Drift Kings RP🏁",
   type: "PLAYING",
   time: 12
 })
@@ -16,7 +16,7 @@ bot.onMessage()
 
  // Variable
 bot.variables({
- prefix: "YOUR BOT PREFIX",
+ prefix: "+",
  warn: "0"
 })
 
